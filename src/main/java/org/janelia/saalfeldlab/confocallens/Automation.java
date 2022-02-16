1bed8ff9f77c65fd9cb4b5e519238f5d5ee59243
takashi310 <ymrnztm310@gmail.com>
2022-02-16 13:48:05 -0500

    add steps as comments

--- src/main/java/org/janelia/saalfeldlab/confocallens/Automation.java
+++ src/main/java/org/janelia/saalfeldlab/confocallens/Automation.java
@@ -1,8 +1,34 @@
 package org.janelia.saalfeldlab.confocallens;
 
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.Opener;
+
+import java.io.File;
+import java.util.Arrays;
+
 public class Automation {
 	public static void main(String[] args)
 	{
 		System.out.print("hi");
+		
+		//read lsm files
+		
+		//generate mips
+		
+		//normalize local contrast brx 127 bry 127 stds 3.0 (all layers)
+		
+		//montage all layers. least square, translation.
+		
+		//Align layers. least square (Elastic)
+		
+		//Auto resize canvas
+		
+		//Lens correction (All layers)
+		
+		//Align layers. least square (Elastic)
+		
+		//output coordinate transform
+
 	}
 }
